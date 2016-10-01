9e53e7a98258aef8fe93959cfd427eabb146cd95
Raul G Luna Jr <juniorluna2@gmail.com>
2016-09-30 22:32:02 -0500

    created directories, notes from meeting on 09/30/16

---
+++ iOS/knitit_code_name/knitit_code_name/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  knitit_code_name
+//
+//  Created by Raul G Luna Jr on 9/20/16.
+//  Copyright © 2016 VCRL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
